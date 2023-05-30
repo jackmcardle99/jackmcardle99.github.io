7478e7e1409a9efb07672f656dd63fc5c6ae8723
Jack McArdle <66914827+jackmcardle99@users.noreply.github.com>
2023-05-30 18:19:57 +0100

    added navbar, fiddled around with table styling

--- dist/app.js
+++ dist/app.js
@@ -6,12 +6,15 @@ dropdown.addEventListener("change",handleDropdown);
 
 function handleDropdown(){
     var e = document.getElementById("ddl").value;
-    if (e == "bulking"){
-        updateTableBulking();
+    if(e != "default"){
+        if (e == "bulking"){
+            updateTableBulking();
+        }
+        else{
+            updateTableShredding();
+        }
     }
-    else{
-        updateTableShredding();
-    }
+    
 }
 
 
@@ -38,26 +41,27 @@ function updateTableBulking(){
         row.classList.add("border-b");
         row.classList.add("text-black");
         var cell1 = row.insertCell(0);
+        // var cell2 = row.insertCell(1);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
         
         cell1.innerHTML = (i);
-        cell2.innerHTML = "x" + multiplier;
-        cell3.innerHTML = Math.round(calories) + " kcal";
-        cell4.innerHTML = Math.round(carbInGrams) + "g";
-        cell5.innerHTML = Math.round(fatInGrams) + "g";
+        // cell2.innerHTML = "x" + multiplier;
+        cell2.innerHTML = Math.round(calories) + " kcal";
+        cell3.innerHTML = Math.round(carbInGrams) + "g";
+        cell4.innerHTML = Math.round(fatInGrams) + "g";
 
         cell1.classList.add("px-6")
         cell1.classList.add("py-4")
+        // cell2.classList.add("px-6")
+        // cell2.classList.add("py-4")
         cell2.classList.add("px-6")
         cell2.classList.add("py-4")
         cell3.classList.add("px-6")
-        cell4.classList.add("py-4")
+        cell3.classList.add("py-4")
         cell4.classList.add("px-6")
-        cell5.classList.add("py-4")
-        cell5.classList.add("px-6")
+        cell4.classList.add("py-4")
         multiplier = multiplier + 2;
         if(multiplier==32) multiplier=22;
     }
@@ -86,24 +90,24 @@ function updateTableShredding(){
         
         var row = table.insertRow(i);
         var cell1 = row.insertCell(0);
+        // var cell2 = row.insertCell(1);
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
-        var cell5 = row.insertCell(4);
         
         cell1.innerHTML = (i);
-        cell2.innerHTML = "x" + multiplier;
-        cell3.innerHTML = calories + " kcal";
-        cell4.innerHTML = Math.round(carbInGrams) + "g";
-        cell5.innerHTML = Math.round(fatInGrams) + "g";
+        // cell2.innerHTML = "x" + multiplier;
+        cell2.innerHTML = calories + " kcal";
+        cell3.innerHTML = Math.round(carbInGrams) + "g";
+        cell4.innerHTML = Math.round(fatInGrams) + "g";
 
 
         multiplier = multiplier - 2;
         if(i==11){
             cell1.innerHTML = i + " (optional)";
-            cell2.innerHTML = "x" + 14;
-            cell3.innerHTML = (inputKG * 14) + " kcal";
-            cell4.innerHTML = Math.round((((inputKG * 14) - (proteinInGrams - 4) / 2) / 4)) + "g";
+            // cell2.innerHTML = "x" + 14;
+            cell2.innerHTML = (inputKG * 14) + " kcal";
+            cell3.innerHTML = Math.round((((inputKG * 14) - (proteinInGrams - 4) / 2) / 4)) + "g";
             cell4.innerHTML = Math.round((((inputKG * 14) - (proteinInGrams - 4) / 2) / 9)) + "g";
             multiplier = 22;
         }
